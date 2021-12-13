7e9e4b1aba22ca1c2e388ed69884e6e44068f7e2
lyrical1112 <lyrical1112@naver.com>
2021-12-13 17:23:07 +0900

    모달창 만들기
    클릭하면 반응하는 모달창 (UI) 만들기

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '박스오피스 순위']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, modal변경] = useState(false);
 
   // function 제목바꾸기(){
   //   var newArray = [...글제목];
@@ -35,7 +36,16 @@ function App() {
         <p>2월 19일 발행</p>
         <hr/>
       </div>
-      <Modal></Modal>
+
+      <button onClick={ ()=>{ modal변경(true) } }>모달 창</button>
+
+      {
+        // 삼향연산자 
+        modal === true
+        ? <Modal></Modal>
+        : null
+      }
+      
     </div>
   );
 }
