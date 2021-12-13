8fd0c32e5d69daa993e25ca4ab56e5f48686a1ef
lyrical1112 <lyrical1112@naver.com>
2021-12-13 22:44:15 +0900

    props 활용한 상세페이지
    (UI 제작 패턴) props 활용한 상세페이지

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '박스오피스 순위']);
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false);
+  let [누른제목 , 누른제목변경] = useState(0);
 
 
   // function 제목바꾸기(){
@@ -24,22 +25,25 @@ function App() {
       {/* <button onClick={ 제목바꾸기 }>버튼</button> */}
       
       {
-        글제목.map(function(a){
+        글제목.map(function(a, i){
           return (
             <div className="list">
-              <h3> { a }  <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
+              <h3 onClick={ ()=>{ 누른제목변경(i) } }> { a }  <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
               <p>2월 18일 발행</p>
               <hr/>
             </div>
           )
         })
       }
+      {/* <button onClick={ ()=>{ 누른제목변경(0) } }>버튼1</button>
+      <button onClick={ ()=>{ 누른제목변경(1) } }>버튼2</button>
+      <button onClick={ ()=>{ 누른제목변경(2) } }>버튼3</button> */}
 
-      <button onClick={ ()=>{ modal변경(true) } }>열고닫기</button>
+      <button onClick={ ()=>{ modal변경(!modal) } }>열고닫기</button>
       {
         // 삼향연산자 
         modal === true
-        ? <Modal 글제목={글제목} ></Modal>
+        ? <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
         : null
       }
       
@@ -51,7 +55,7 @@ function Modal(props){
   return(
     <>
       <div className='modal'>
-        <h2>{ props.글제목[0] }</h2>
+        <h2>{ props.글제목[props.누른제목] }</h2>
         <p>날짜</p>
         <p>상세내용</p>
       </div>
