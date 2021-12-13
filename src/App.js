846bbcf543695535236dd4082d3d042454ba8803
lyrical1112 <lyrical1112@naver.com>
2021-12-13 14:58:24 +0900

    state 사용
    중요한 데이터는 state에 사용한다

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import React , { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  let posts = '강남 고기 맛집';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '박스오피스 순위']);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ {color: 'blue' , fontSize : '30px'} }>개발 Blog</div>
+        <div>개발 Blog</div>
       </div>
-      <h4> { posts } </h4>
+      <div className="list">
+        <h3> { 글제목[0] } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[1] } </h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[2] } </h3>
+        <p>2월 19일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
