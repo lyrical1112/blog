2135781ace65ccdefd29e08d6cedd3d2348d7a25
lyrical1112 <lyrical1112@naver.com>
2021-12-13 16:41:15 +0900

    컴포넌트
    컴포넌트 생성 및 사용법

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '박스오피스 순위']);
   let [따봉, 따봉변경] = useState(0);
 
-  function 제목바꾸기(){
-    var newArray = [...글제목];
-    newArray[0] = '여자 코트 추천';
-    글제목변경( newArray );
-  }
+  // function 제목바꾸기(){
+  //   var newArray = [...글제목];
+  //   newArray[0] = '여자 코트 추천';
+  //   글제목변경( newArray );
+  // }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ 제목바꾸기 }>버튼</button>
+      {/* <button onClick={ 제목바꾸기 }>버튼</button> */}
       <div className="list">
         <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
@@ -35,8 +35,21 @@ function App() {
         <p>2월 19일 발행</p>
         <hr/>
       </div>
+      <Modal></Modal>
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <>
+      <div className='modal'>
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
+  )
 }
 
 export default App;
